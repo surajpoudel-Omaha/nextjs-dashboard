a2c26c053ca16c21a24cc07a159f1eb9336eab45
himatrek <surajpoudel@unomaha.edu>
2025-01-19 17:42:22 -0600

    new update

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -17,7 +17,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -34,8 +34,8 @@ export default async function LatestInvoices({
                     src={invoice.image_url}
                     alt={`${invoice.name}'s profile picture`}
                     className="mr-4 rounded-full"
-                    width={32}
-                    height={32}
+                    width={60}
+                    height={60}
                   />
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
@@ -54,7 +54,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
